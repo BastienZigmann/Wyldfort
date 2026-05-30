65bcf7d41e279eb1afcbe69bc3eef01f1aa59655
Bastien <bastien@zigmann.org>
2026-05-30 10:23:38 +0200

    Base behavior state

---
+++ Source/Wyldfort/Public/Core/Bases/BaseBehaviorState.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BaseBehaviorState.generated.h"
+
+class ABaseActor;
+
+
+UCLASS()
+class WYLDFORT_API UBaseBehaviorState : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	UBaseBehaviorState() {};
+	void Init(ABaseActor* InOwner) { Owner = InOwner; };
+protected:
+	virtual void Enter() {};
+	virtual void Update(float DeltaTime) {};
+	virtual void Exit() {};
+	
+	template<typename T>
+	T* GetOwner() const {return Cast<T>(Owner);}
+	
+	enum class ESubState : uint8 {NONE, GOTO, DO, GO_BACK};
+	ESubState SubState = ESubState::NONE;
+
+private:
+	UPROPERTY()
+	TObjectPtr<ABaseActor> Owner;
+
+};
